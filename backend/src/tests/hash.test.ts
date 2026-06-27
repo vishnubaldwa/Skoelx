766e203bb4d527087770487265799decc5a879bc
vishnubaldwa <vishnu.baldwa@yahoo.com>
2026-06-27 13:14:18 +0530

    feat(auth): add password hashing utility

---
+++ backend/src/tests/hash.test.ts
@@ -0,0 +1,16 @@
+import { hashPassword, verifyPassword } from "../utils/hash.js";
+
+async function main() {
+  const password = "Admin@123";
+
+  const hash = await hashPassword(password);
+
+  console.log("Hash:");
+  console.log(hash);
+
+  const valid = await verifyPassword(password, hash);
+
+  console.log("Password Match:", valid);
+}
+
+main();
